f912263afd43fbfda22eecf45ebed02bd2bb2689
Alexander Wauck <alex.wauck@fpx.com>
2014-06-25 14:37:44 -0500

    Whoops, forgot to bump the version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cpqpython',
-    version='0.0.4',
+    version='0.0.5',
     description='Python wrapper for CPQ REST API',
     long_description='',
     author='Cameron Stitt',
